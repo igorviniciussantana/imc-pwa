970368fef5f6dbd47b65530911cb77cd7990a171
igor vinicius <igor.santana@estudante.ifms.edu.br>
2022-08-25 22:15:37 -0400

    Adicionado Gerador de PDF

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-window.onload = () => {
-    "use strict";
-    if ("serviceWorker" in navigator) {
-        navigator.serviceWorker.register("./sw.js");
-    }
-};
+// window.onload = () => {
+//     "use strict";
+//     if ("serviceWorker" in navigator) {
+//         navigator.serviceWorker.register("./sw.js");
+//     }
+// };
 
 
 function calcular() {
@@ -13,7 +13,7 @@ function calcular() {
     const sexo = document.getElementById("sexo").value;
     const peso = document.getElementById("peso").value;
     const altura = document.getElementById("altura").value;
-    const cima = document.getElementById("cima");
+   
 
 
 
@@ -21,9 +21,9 @@ function calcular() {
 
 
     nome_tela.textContent = nome + ", ";
-    idade_tela.textContent = ` ${idade} anos`;
+    idade_tela.textContent =  idade + " anos";
     sexo_tela.textContent = sexo;
-   altura_tela.textContent = altura + "m - ";
+   altura_tela.textContent = altura + "m -";
    peso_tela.textContent = peso + "kg";
     resultado.textContent = soma.toFixed(2);
 
@@ -66,17 +66,69 @@ function limpar() {
 
 }
 
+// function downloadPDF() {
+//     const item = document.querySelector(".resultados");
+//     const nome = document.getElementById("nome").value;
+//     const fileName = nome + '.pdf'
+  
+//     var opt = {
+//       margin: 0,
+//       filename: fileName,
+//       html2canvas: { scale: 2},
+//       jsPDF: { unit: "cm", format: "letter", orientation: "portrait" },
+//     };
+  
+//     html2pdf().set(opt).from(item).save();
+//   }
+
 function downloadPDF() {
     const item = document.querySelector(".resultados");
     const nome = document.getElementById("nome").value;
+    const idade = document.getElementById("idade").value;
+    const sexo = document.getElementById("sexo").value;
+    const peso = document.getElementById("peso").value;
+    const altura = document.getElementById("altura").value;
     const fileName = nome + '.pdf'
+
   
-    var opt = {
-      margin: 1,
-      filename: fileName,
-      html2canvas: { scale: 2 },
-      jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
-    };
+const div = document.createElement('div')
+
+div.insertAdjacentHTML('afterbegin', 
+
+`
+<div class="file">
+<h1 class="fileTitle">Relatório IMC</h1>
+        <div class="cima" id="cima">
+          <div class="nomeIdade">
+            <h1 id="nome_tela">${nome}, </h1>
+            <h1 id="idade_tela">${idade} anos</h1>
+          </div>
+          <h1 id="sexo_tela">${sexo}</h1>
+
+          <div class="pesoAltura">
+            <h1 id="altura_tela">${altura} -</h1>
+            <h1 id="peso_tela"> ${peso}</h1>
+          </div>
+        </div>
+        Seu IMC é:
+        <h1 id="resultado">0</h1>
+        <h1 id="tabela"></h1>
+        <h1 class="generate">Gerado com <span>Calculadora IMC</span></h1>
+      </div>
+
+
+`
+
+
+
+)
   
-    html2pdf().set(opt).from(item).save();
+    html2pdf(div, {
+
+        margin: 0,
+        filename: nome + ".pdf",
+        html2canvas: { scale: 2, backgroundColor: "#ff8dd0" },
+        jsPDF: { unit: "in", format: "letter", orientation: "portrait" }
+
+    });
   }
